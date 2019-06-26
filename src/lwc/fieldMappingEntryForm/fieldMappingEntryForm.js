9e02756fa0ce38c7b7456a25f75a0039c67ac956
Ken Lewis <kenneth.lewis@salesforce.com>
2019-06-26 09:46:20 -0700

    working functionality for target field options to be responsive to selected value in source field options

--- src/lwc/fieldMappingEntryForm/fieldMappingEntryForm.js
+++ src/lwc/fieldMappingEntryForm/fieldMappingEntryForm.js
@@ -4,6 +4,7 @@
 
 import {LightningElement, track, wire} from 'lwc';
 import getSourceFieldInfos from '@salesforce/apex/FieldMappingController.getSourceFieldInfos';
+import getTargetFieldInfos from '@salesforce/apex/FieldMappingController.getTargetFieldInfos';
 
 export default class FieldMappingEntryForm extends LightningElement {
     @track sourceFieldName = '';
@@ -12,99 +13,111 @@ export default class FieldMappingEntryForm extends LightningElement {
     @track value = 'Select a source field';
     @track options = [];
 
-    // @wire(getSourceFieldInfos({ objectApiName:'DataImport__c' })) options;
-
     constructor(){
         super();
         getSourceFieldInfos({objectApiName: 'DataImport__c'})
             .then((data) => {
-                let x = [];
-                data.forEach((item) => {
-                    let option = {
-                        label: item.label,
-                        value: item.name
-                    };
-                    console.log('*** ' + 'option!' + ' ***');
-                    console.log(JSON.stringify(option));
-                    x.push(option);
-                });
-                console.log('*** ' + 'setting options' + ' ***');
-                this.options = x;
+                this.options = this.getOptions(data);
+            });
+
+        getTargetFieldInfos(
+            {
+                sourceObjectName: 'DataImport__c',
+                sourceFieldName: 'Name',
+                targetObjectName: 'Contact'
+            })
+            .then((data) => {
+                // todo: this.initialize(data); //avoid looping twice
+                this.targetOptions = this.getOptions(data);
+                this.setDisplayTypeByFieldName();
             });
     }
-    // get options() {
-    //     let options = [];
-    //     getSourceFieldInfos({ objectApiName:'DataImport__c' })
-    //         .then((data) => {
-    //
-    //             // return [{label:'test',value:'test'}];
-    //
-    //             for (let i = 0; i < 10; i++) {
-    //                 let datum = data[i];
-    //                 let option = {
-    //                     label: datum.label,
-    //                     value: datum.name
-    //                 };
-    //                 console.log('*** ' + 'option!' + ' ***');
-    //                 console.log(JSON.stringify(option));
-    //                 options.push(option);
-    //             }
-    //             console.log('*** ' + 'returning after for loop' + ' ***');
-    //             // return options;
-    //             //
-    //             // data.forEach((item) => {
-    //             //     let option = {
-    //             //         label: item.label,
-    //             //         value: item.name
-    //             //     };
-    //             //     console.log('*** ' + 'option!' + ' ***');
-    //             // console.log(JSON.stringify(option));
-    //             //     options.push(option);
-    //             // });
-    //             // console.log('*** ' + 'options:' + ' ***');
-    //             // console.log(JSON.stringify(options));
-    //             //
-    //             // // debugger;
-    //             // return options;
-    //             // let options = this.addMapsToIconProperty(data);
-    //             // console.log('received data: ', data);
-    //             // console.log('fieldMappings: ', this.fieldMappings);
-    //     console.log('*** ' + 'options' + ' ***');
-    //             console.log(JSON.stringify(options));
-    //     return options;
-    //         })
-    //         .catch((error) => {
-    //             this.message = 'Error received: code' + error.errorCode + ', ' +
-    //                 'message ' + error.body.message;
-    //             console.log(this.message);
-    //         });
-    //
-    //
-    //     // options [] = getSourceFieldInfos({objectApiName:'DataImport__c'});
-    //     //
-    //     // return [
-    //     //     { label: 'New', value: 'new' },
-    //     //     { label: 'In Progress', value: 'inProgress' },
-    //     //     { label: 'Finished', value: 'finished' },
-    //     // ];
-    // }
+
+    setDisplayTypeByFieldName = () => {
+        console.log('*** ' + 'setting displaytypesbyfieldname' + ' ***');
+            this.displayTypesByFieldName = new Map();
+        this.targetOptions.forEach((item) => {
+            console.log(item);
+            // this.displayTypesByFieldName[item.value] = item.displayType;
+            this.displayTypesByFieldName.set(item.value, item.displayType);
+        });
+        console.log('this.displayTypesByFieldName: ', this.displayTypesByFieldName);
+    }
+
+    getOptions = (data) => {
+        let x = [];
+        data.forEach((item) => {
+            let option = {
+                label: item.label,
+                value: item.name,
+                displayType: item.displayType
+            };
+            x.push(option);
+        });
+        return x;
+    }
+
+    // getOptionsByName =
 
     handleChange(event) {
+        // console.log(JSON.stringify(event));
+        console.log('*** ' + 'in handleChange' + ' ***');
+        console.log('event.detail.value: ', event.detail.value);
         this.value = event.detail.value;
+        console.log('this.displayTypesByFieldName: ' + this.displayTypesByFieldName);
+        let sourceFieldType = this.displayTypesByFieldName.get(this.value);
+        console.log('sourceFieldType: ', sourceFieldType);
+        this.validTargetOptions = this.targetOptions.filter(this.isValidTargetMapping(sourceFieldType));
+        console.log('*** ' + 'after filtering' + ' ***');
+        console.log('this.validTargetOptions: ', this.validTargetOptions);
+        console.log('*** ' + 'end handleChange' + ' ***');
     }
 
-
-    @track value2 = 'inProgress2';
-
-    get options2() {
-        return [
-            { label: 'New2', value: 'new2' },
-            { label: 'In Progress2', value: 'inProgress2' },
-            { label: 'Finished2', value: 'finished2' },
-        ];
-    }
+    @track value2 = 'target field options...';
+    targetOptions = [];
+    displayTypesByFieldName;
+    @track validTargetOptions = [];
 
     handleChange2(event) {
         this.value2 = event.detail.value;
     }
+
+    isValidTargetMapping = (sourceType) => {
+        return (targetFieldInfo) => {
+            console.log('*** ' + 'in returned function' + ' ***');
+            console.log(sourceType);
+            console.log(targetFieldInfo);
+            if (targetFieldInfo.displayType === sourceType) {
+                return true;
+            }
+            //future implementation of multiple target field types
+            switch (targetFieldInfo.displayType) {
+                case 'ADDRESS':
+                case 'BASE64':
+                case 'BOOLEAN':
+                case 'LIST':
+                case 'CURRENCY':
+                case 'DATACATEGORYGROUPREFERENCE':
+                case 'DATE':
+                case 'DATETIME':
+                case 'DOUBLE':
+                case 'EMAIL':
+                case 'ENCRYPTEDSTRING':
+                case 'ID':
+                case 'INTEGER':
+                case 'LONG':
+                case 'SELECTED':
+                case 'PERCENT':
+                case 'NUMBER':
+                case 'SELECTED':
+                case 'REFERENCE':
+                case 'STRING':
+                case 'TEXTAREA':
+                case 'TIME':
+                case 'URLCASE':
+                default:
+            }
+        }
+    }
+
 }
